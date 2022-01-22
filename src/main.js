f3e7442f6678a8484d60d597bf1e7b1f42abb51a
Ardhanari <genevril@gmail.com>
2022-01-22 14:34:05 +0000

    clean up file architecture

---
+++ src/main.js
@@ -0,0 +1,41 @@
+var phrase;
+var lettersUsed;
+var lettersFound;
+var lettersNotFound;
+
+
+phrase = document.getElementById('phrase');
+
+console.log(phrase)
+
+
+// document.getElementById('contents'); //returns a HTML DOM Object
+
+// var contents = $('#contents');  //returns a jQuery Object
+// In jQuery, to get the same result as document.getElementById, you can access the jQuery Object and get the first element in the object (Remember JavaScript objects act similar to associative arrays).
+
+// var contents = $('#contents')[0]; //returns a HTML DOM Object
+
+function helloWorld() { 
+    return "hello world!"; 
+}
+
+function setPolishLanguage() { 
+    console.log("Polish")
+}
+
+function setSpanishLanguage() { 
+    console.log("Espagnol")
+}
+
+function setEnglishLanguage() { 
+    console.log("Eng")
+}
+
+function obscurePhrase() {
+
+}
+
+function showResult() {
+    
+}
